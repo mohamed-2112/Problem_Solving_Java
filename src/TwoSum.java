df1b2ccd23e5ddaf3e0676653e0de1e851fb6100
mohamed-2112 <mohamed.rasmy.fathy@gmail.com>
2024-01-21 18:38:46 -0500

    add the two sum code

--- src/TwoSum.java
+++ src/TwoSum.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class TwoSum {
+import java.util.HashMap;
+
+public class TwoSum {
+
+    public int[] twoSum(int[] nums, int target){
+        HashMap<Integer, Integer> numMap = new HashMap<>();
+        int compliment;
+
+        for (int i=0; i<nums.length; i++){
+            compliment = target - nums[i];
+            if (numMap.containsKey(compliment)){
+                return new int[] {numMap.get(compliment), i};
+            }
+            numMap.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
 }
